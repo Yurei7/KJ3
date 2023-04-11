b87265b0b16caa4c778233bf8bee47f15960de9c
colepfeiffer.sol <114104437+Yurei7@users.noreply.github.com>
2023-04-11 09:35:11 -0400

    1.1

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-let numOfFlipBtns = document.querySelectorAll(".flipbtn").length;
+
 let HEADS = "Heads";
 let TAILS = "Tails";
-
-for (i = 0; i < numOfFlipBtns; i++) {
-  document.querySelectorAll(".flipbtn")[i].addEventListener("click", flip)
-}
-
+$(".flipbtn").click(flip)
 function flip() {
-  let medHeading = document.querySelector(".medium-heading")
-  let winOrLosePrompt= document.querySelector(".winorlose")
+  let medHeading = $(".medium-heading")
+  let winOrLosePrompt = $(".winorlose")
   let randomNum = Math.floor((Math.random() * 2));
   let options = [HEADS, TAILS];
   let result = options[randomNum];
   let playerChoice = this.innerHTML
   if (playerChoice === result) {
-    medHeading.textContent = result
-    winOrLosePrompt.textContent="WINNER WINNER CHICKEN DINNER";
-    winOrLosePrompt.style.visibility="visible";
+    medHeading.text(result);
+    winOrLosePrompt.text("WINNER WINNER CHICKEN DINNER");
+    winOrLosePrompt.css("visibilityy", "visible");
   }
   else if (playerChoice != result) {
-    medHeading.textContent = result;
-    winOrLosePrompt.textContent= "LOSER LOSER DRUG ABUSER";
-    winOrLosePrompt.style.visibility= "visible";
-    
+    medHeading.text(result);
+    winOrLosePrompt.text("LOSER LOSER DRUG ABUSER");
+    winOrLosePrompt.css("visibility", "visible");
   }
 }
 
-/*
-document.addEventListener("keydown",function (e){
-  console.log (e.key);
-})
-*/
