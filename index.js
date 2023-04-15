683a9ce99fd2b23852f23090f70a7ffa440ac8c2
Yurei7 <114104437+Yurei7@users.noreply.github.com>
2023-04-15 09:02:24 -0400

    1.1

--- index.js
+++ index.js
@@ -4,20 +4,43 @@ let TAILS = "Tails";
 $(".flipbtn").click(flip)
 function flip() {
   let medHeading = $(".medium-heading")
-  let winOrLosePrompt = $(".winorlose")
   let randomNum = Math.floor((Math.random() * 2));
   let options = [HEADS, TAILS];
   let result = options[randomNum];
   let playerChoice = this.innerHTML
-  if (playerChoice === result) {
-    medHeading.text(result);
-    winOrLosePrompt.text("WINNER WINNER CHICKEN DINNER");
-    winOrLosePrompt.css("visibilityy", "visible");
-  }
-  else if (playerChoice != result) {
-    medHeading.text(result);
-    winOrLosePrompt.text("LOSER LOSER DRUG ABUSER");
-    winOrLosePrompt.css("visibility", "visible");
-  }
+
+  $(".loading").css("visibility", "visible");
+  $(".flipscreen").css("visibility", "hidden");
+  medHeading.text("FLIPPING...")
+  setTimeout(() => {
+    $(".loading").css("visibility", "hidden")
+    $(".flipscreen").css("visibility", "visible")
+
+
+    if (result === HEADS) {
+      $(".heads-img").css("visibility", "visible")
+      $(".tails-img").css("visibility", "hidden")
+    }
+    else if (result === TAILS) {
+      $(".tails-img").css("visibility", "visible")
+      $(".heads-img").css("visibility", "hidden")
+    }
+
+    if (playerChoice === result) {
+      /* medHeading.text(result);*/
+      medHeading.text("WINNER WINNER CHICKEN DINNER");
+      medHeading.css("font-size", "50px")
+      /*winOrLosePrompt.css("visibilityy", "visible");*/
+    }
+    else if (playerChoice != result) {
+      /*medHeading.text(result);*/
+      medHeading.text("LOSER LOSER DRUG ABUSER");
+      medHeading.css("font-size", "50px")
+
+    }
+
+  }, 3000);
 }
+
+
 
